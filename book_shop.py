1b1af55f5d9490ed40cd4ac046553df16f4f8628
AiConnectHub <aiconnecthub01@gmail.com>
2024-03-27 18:49:18 +0600

    Updated 27/03/24

--- book_shop.py
+++ book_shop.py
@@ -7,7 +7,7 @@ class Library:
     def __init__(self):
         self.books = []
 
-    # Loading File From Here
+    # 
     def load_books(self):
         try:
             with open('books.json', 'r') as file:
@@ -16,17 +16,15 @@ class Library:
             logging.warning('No book data found.')
             self.books = []
 
-    # Save File From Here
     def save_books(self):
         with open('books.json', 'w') as file:
             json.dump(self.books, file, indent=4)
 
-    # Add Book to the Libraries
     def add_book(self):
         while True:
             title = input("Enter book title: ")
             author = input("Enter book author: ")
-            book_id = int(input("Enter book ID: "))
+            book_id = input("Enter book ID: ")
             published_year = input("Enter published year: ")
             book = {'title': title, 'author': author, 'id': book_id, 'published_year': published_year}
             
@@ -38,32 +36,46 @@ class Library:
                 self.save_books()
                 break
 
-    # Updated Book 
     def update_book(self):
-        book_id = int(input("Enter book ID to update: "))
-        title = input("Enter new book title: ")
-        author = input("Enter new book author: ")
-        new_id = input("Enter new book ID: ")
-        published_year = input("Enter new published year: ")
-        new_book = {'title': title, 'author': author, 'id': new_id, 'published_year': published_year}
-        try:
-            self.books[book_id] = new_book
-            logging.info(f"Updated book: {new_book['title']} by {new_book['author']}")
-            self.save_books()
-        except IndexError:
+        book_id = input("Enter book ID to update: ")
+        found = False
+        for book in self.books:
+            if book['id'] == book_id:
+                try:
+                    title = input("Enter new book title: ")
+                    author = input("Enter new book author: ")
+                    new_id = input("Enter new book ID: ")
+                    published_year = input("Enter new published year: ")
+                    book.update({'title': title, 'author': author, 'id': new_id, 'published_year': published_year})
+                    logging.info(f"Updated book: {book['title']} by {book['author']}")
+                    found = True
+                except Exception as e:
+                    logging.error(f"Error updating book: {e}")
+                break
+        
+        if not found:
             logging.error(f"Book with ID {book_id} not found.")
 
-    # Delete The Book
+        self.save_books()
+
     def delete_book(self):
-        book_id = int(input("Enter book ID to delete: "))
-        try:
-            deleted_book = self.books.pop(book_id)
-            logging.info(f"Deleted book: {deleted_book['title']} by {deleted_book['author']}")
-            self.save_books()
-        except IndexError:
+        book_id = input("Enter book ID to delete: ")
+        found = False
+        for book in self.books:
+            if book['id'] == book_id:
+                try:
+                    self.books.remove(book)
+                    logging.info(f"Deleted book: {book['title']} by {book['author']}")
+                    found = True
+                except Exception as e:
+                    logging.error(f"Error deleting book: {e}")
+                break
+        
+        if not found:
             logging.error(f"Book with ID {book_id} not found.")
 
-    # Display All The Item
+        self.save_books()
+
     def display_books(self):
         if not self.books:
             print("No books available.")
@@ -73,18 +85,25 @@ class Library:
             for i, book in enumerate(self.books):
                 print(f"{i}: {book['title']} by {book['author']}, ID: {book['id']}, Published Year: {book['published_year']}")
 
-    # lend Book 
     def lend_book(self):
-        book_id = int(input("Enter book ID to lend: "))
-        user = input("Enter user name: ")
-        try:
-            lent_book = self.books.pop(book_id)
-            logging.info(f"Lent book: {lent_book['title']} by {lent_book['author']} to {user}")
-            self.save_books()
-        except IndexError:
+        book_id = input("Enter book ID to lend: ")
+        found = False
+        for book in self.books:
+            if book['id'] == book_id:
+                try:
+                    user = input("Enter user name: ")
+                    logging.info(f"Lent book: {book['title']} by {book['author']} to {user}")
+                    self.books.remove(book)
+                    found = True
+                except Exception as e:
+                    logging.error(f"Error lending book: {e}")
+                break
+        
+        if not found:
             logging.error(f"Book with ID {book_id} not found.")
 
-    # Return book 
+        self.save_books()
+
     def return_book(self):
         title = input("Enter book title to return: ")
         author = input("Enter book author to return: ")
@@ -96,7 +115,6 @@ class Library:
         self.save_books()
 
 
-# class and all def will call from here
 def main():
     library = Library()
     library.load_books()
